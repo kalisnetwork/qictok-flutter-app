d6d5b7a57166cc037519abb245ad0f3afccf9fd0
Nagwswara Rao <nagwswararao@Lakshmis-Laptop.local>
2026-03-11 16:53:54 +0530

    chore: remove non-mobile platforms and finalize production screen suite

---
+++ README.md
@@ -0,0 +1,3 @@
+# latest_flutter_template
+
+A new Flutter project.
--- android/app/src/main/kotlin/com/digitalleadpro/qictok_mobile/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.digitalleadpro.qictok_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
